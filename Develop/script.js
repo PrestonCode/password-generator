ec502e440edec05a7c68bd45a58fe23182e25c67
Preston Patterson <prestonjpatterson@gmail.com>
2020-06-26 17:17:22 -0600

    added the password generater v1

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,42 @@
 // Assignment Code
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
-var symbols = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+var letters = "abcdefghijklmnopqrstuvwxyz"
+var numbers = "1234567890"
+var symbols = " !#$%&'()* -./:;<=>?@[]^_`{|}~"
 var generateBtn = document.querySelector("#generate");
 var numbersSelect = confirm("Do you want numbers in your password?")
 var symbolsSelect = confirm("Do you want symbols in your password?")
-var lettersSelect = confirm("Do you want letters in your password?")
-if (lettersSelect === true) {
-    var lowerSelect = confirm("Do you want lower case letters in your password?")
-    var upperSelect = confirm("Do you want upper case letters in your password?")
-}
+var lowerSelect = confirm("Do you want lower case letters in your password?")
+var upperSelect = confirm("Do you want upper case letters in your password?")
+var length = prompt("How long do you want your passworld?")
 // test if this code works
-var passwordPieces = []
-function randomizer(character) {
-    push.passwordPieces(Math.round((Math.random() * (character.length))));
-
-}
+var userAnser = "" ;
+var passwordPieces = [];
 function generatePassword () {
-    if (numbersSelect === true && symbolsSelect === true && lettersSelect === true && lowerSelect === true && upperSelect === true) {
-        randomizer(letters)
+    
+    }if (numbersSelect === true) {
+        var userAnser = (userAnser + numbers)
     }
-}
+    if (true && symbolsSelect); {
+        var userAnser = (userAnser + symbols)
+    }
+    if (lowerSelect === true); {
+        var userAnser = (userAnser + letters)
+    }
+    if (upperSelect === true) {
+        var userAnser = (userAnser + (letters.toUpperCase))
+    }
+    for (let index = 0; index < length; index++) {
+        var passwordNumbers = Math.round((Math.random() * (userAnser.length)));
+        passwordPieces.push(passwordNumbers)
+    }
+    
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    generatePassword()
+    var password = passwordPieces.toString();;
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
